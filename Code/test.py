bbe7a833d035f10a5563222e836df39687e5a6b7
Johnson <chenheng@tju.edu.cn>
2016-10-04 14:09:40 +0800

    百度贴吧图片下载爬虫

--- Code/test.py
+++ Code/test.py
@@ -95,29 +95,78 @@
 
 # #————————————————————————————————————————————————————————————————
 
-#对函数的封装
-import urllib2
-import random
+# #对函数的封装
+# import urllib2
+# import random
 
-url = "http://blog.csdn.net/happydeer"
-my_headers=[
- 	" Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
-]
+# url = "http://blog.csdn.net/happydeer"
+# my_headers=[
+#  	" Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
+# ]
 
 
 
-def get_content(url,headers):
-	"""
-	@获取403禁止访问的网页
-	"""
-	random_header = random.choice(headers)
-	req = urllib2.Request(url)
-	req.add_header("User-Agent",random_header)
-	req.add_header("Host","blog.csdn.net")
-	req.add_header("Referer","http://blog.csdn.net/")
-	req.add_header("GET",url)
+# def get_content(url,headers):
+# 	"""
+# 	@获取403禁止访问的网页
+# 	"""
+# 	random_header = random.choice(headers)
+# 	req = urllib2.Request(url)
+# 	req.add_header("User-Agent",random_header)
+# 	req.add_header("Host","blog.csdn.net")
+# 	req.add_header("Referer","http://blog.csdn.net/")
+# 	req.add_header("GET",url)
 
-	content = urllib2.urlopen(req).read()
-	return content
+# 	content = urllib2.urlopen(req).read()
+# 	return content
 
-print get_content(url,my_headers)
+# print get_content(url,my_headers)
+
+# #————————————————————————————————————————————————————————————————
+
+
+# #百度贴吧的图片下载爬虫
+# #http://tieba.baidu.com/p/2772656630
+
+# import re   #正则表达式模块
+# import urllib
+
+# def get_content(url):
+#     """ doc."""
+#     html = urllib.urlopen(url)
+#     content = html.read()
+#     html.close()
+
+#     return content
+
+
+
+# def get_images(info):
+#     """doc
+#      <img class="BDE_Image" src="http://imgsrc.baidu.com/forum/w%3D580/
+#      sign=ab00caa474094b36db921be593cd7c00/e3c551da81cb39db2c50532dd2160924ab18303d.jpg" 
+#      pic_ext="jpeg" changedsize="true" width="560" height="840">
+
+#     """
+#     #pass             #占位
+#     #点号匹配除换行符之外的任意字符，+号标示一个或多个，?号标示尽量少重复,反斜杠转义
+#     regex = r'class="BDE_Image" src="(.+?\.jpg)"'
+#     pat = re.compile(regex)
+
+#     images_code = re.findall(pat,info)
+
+#     i = 0
+
+#     for image_url in images_code:
+#     	print image_url
+
+#     	urllib.urlretrieve(image_url,'./image/%s.jpg' % i)
+#     	i += 1
+
+
+#     return images_code
+
+# info =  get_content("http://tieba.baidu.com/p/2772656630")
+# get_images(info)
+
+# #————————————————————————————————————————————————————————————————
