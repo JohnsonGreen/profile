ca3fdacbd83bc6f605af1641c25dfa1d5579e680
Johnson <chenheng@tju.edu.cn>
2016-10-29 22:39:33 +0800

    Add some Hights class

--- Code/AndroidStudioProjects/addB/app/src/main/java/com/example/cyh/addb/MainActivity.java
+++ Code/AndroidStudioProjects/addB/app/src/main/java/com/example/cyh/addb/MainActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.os.Message;
 import android.os.Handler;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -23,6 +26,7 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -38,22 +42,29 @@ public class MainActivity extends Activity {
     //获取手机屏幕分辨率的类
     private DisplayMetrics dm;
     private Handler handler = null;
-    private Integer remain_height = 0;
+    private int remain_height = 0;
     private RelativeLayout lay = null;
-    private Integer remain_width = 0;
-    private Integer margin = 0;
+    private int remain_width = 0;
+    private int margin = 0;
+    private int hole_margin = 0;
     private Mqtt mqListen = null;
     private Mqtt mqSend = null;
-    private int button_width = 58;
-    private int button_height = 58;
+
     private int hole_width = 80;
     private int hole_padding = 10;
     private int hole_id = -1;
     private int enable_id = -1;
     private int enable_hole_id = -1;
     private int btnid = 0;
+    private int button_hight_dp = 70;  //返回键+ 确认键的高度dp
+    private int white_id = 0;
 
-    private ImageButton Btn[] = new ImageButton[20];
+
+
+
+    private  Hights high = new Hights();
+
+    private ImageButton Btn[] = new ImageButton[50];
     private ImageButton holeButton[] = new ImageButton[6];
 
     private boolean hole_enable = false;
@@ -61,7 +72,7 @@ public class MainActivity extends Activity {
 
     //private LinkedHashMap<String,String> coordinate;
 
-    private String[][] coordinate = new String[20][2];
+    private String[][] coordinate = new String[50][2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,20 +80,23 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        System.out.println("+++++++++++++++++++++++++++++++++++");
-             //Mqtt mqini = mqttinit();
-             //mqini.setContent("heheda!!!!!");
-             //mqini.send();
-             //mqini.setTopic("#");
-             //mqini.listen();
+
+
+
+
+        System.out.println("getScreenHeight： " + high.getScreenHeight(this));
+        System.out.println("getBottomStatusHeight： " + high.getBottomStatusHeight(this));
+        System.out.println("getDpi： " + high.getDpi(this));
+        System.out.println("getTitleHeight： " + high.getTitleHeight(this));
+        System.out.println("getStatusHeight： " + high.getStatusHeight(this));
+        other_pixels();
+        getremainHW();
 
 
         mqListen = new mqttListen().mListen("hahahaha");
         mqSend = new mqttSend().mSend("coordinate","Hello");
-
-        System.out.println("-----------------------------------");
         getremainHW();
-        LinkedHashMap<String,String> ma = strToMap("way=0&rec3=100@200@9@syellow&rec2=150@200@9@white&rec4=1100@300@9@bred&end=a");
+        LinkedHashMap<String,String> ma = strToMap("way=0&rec3=1200@320@9@syellow&rec2=440@550@9@white&rec4=1100@300@9@bred&end=a");
         changeUI(ma);
 
         handler = new Handler(){
@@ -93,6 +107,7 @@ public class MainActivity extends Activity {
 
                     System.out.println("shoudao");
                     LinearLayout layout = (LinearLayout)findViewById(R.id.linearBelow);
+
                     //layout.removeAllViews();
                    layout.removeView(lay);    //移除 动态添加的view
                     if( ! mqListen.ret.equals("NoData")){
@@ -105,13 +120,15 @@ public class MainActivity extends Activity {
         mqListen.setHandler(handler);
 
 
+
+
         //1794 x 1080
         //1600 x 1080
         //qiu: 53
         //1366 x 667
+
+        //球桌大小 1488x800
     }
-
-
 
 
     protected  LinkedHashMap toMap(String jsonString) throws JSONException {
@@ -166,77 +183,85 @@ public class MainActivity extends Activity {
     }
 
 
-    protected Integer other_pixels(){
 
-        Integer button_hight = 2 * dip2px(this,50);                      //
+    protected int other_pixels(){
 
-        System.out.println("button_hight : " + button_hight);
+        int button_hight = dip2px(this,button_hight_dp);
+        int statusBarHeight = high.getStatusHeight(this);
 
-        int statusBarHeight = -1;
-//获取status_bar_height资源的ID
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            //根据资源ID获取响应的尺寸值
-            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
-        }
-
-        System.out.println(button_hight);
-        System.out.println(statusBarHeight);
+        System.out.println("button_hight: " + button_hight);
+        System.out.println("statusBarHeight: " + statusBarHeight);
 
         return  button_hight + statusBarHeight;
     }
+
 
     protected void getremainHW(){
         dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-        Integer width = dm.widthPixels;
-        Integer height = dm.heightPixels;
-
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
 
         remain_height = height - other_pixels();
         System.out.println("remain_height" + remain_height);
-        remain_width = (int)(remain_height * 667 / 1366.0f + 0.5f);
+        remain_width = (int)(remain_height * 800 / 1488.0f + 0.5f);
+        System.out.println("remain_width" + remain_width);
         margin = (width - remain_width) / 2;
+        System.out.println("margin" + margin);
     }
 
-    /*
-     * 设置控件所在的位置YY，并且不改变宽高，
-     * XY为绝对位置
-     */
+    protected boolean isChanged(LinkedHashMap<String,String> ma){
 
-    public static void setLayout(View view,int x,int y)
-    {
-        ViewGroup.MarginLayoutParams margin=new ViewGroup.MarginLayoutParams(view.getLayoutParams());
-        margin.setMargins(x,y, x+margin.width, y+margin.height);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(margin);
-        view.setLayoutParams(layoutParams);
+
+
     }
 
 
     protected void changeUI(LinkedHashMap<String,String> ma){
 
+        int lf_border = (int)(75 * remain_height / 1488.0 + 0.5f);
+        int to_border = (int)(87 * remain_height / 1488.0 + 0.5f);
+        int ri_border = (int)(77 * remain_height / 1488.0 + 0.5f);
+        int do_border = (int)(73 * remain_height / 1488.0 + 0.5f);
+        int real_desk_width = remain_width - lf_border -ri_border;
+        int real_desk_hight = remain_height - to_border - do_border;
+
+        int button_width = 40 * real_desk_hight / 1366;
+
+
+
+        System.out.println("real_desk_width: " + real_desk_width);
+        System.out.println("real_desk_hight: " + real_desk_hight);
+
+
+
         LinearLayout layout = (LinearLayout)findViewById(R.id.linearBelow);
         lay = new RelativeLayout(this);
-        ViewGroup.LayoutParams lp_fullWidth =
-                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lay.setBackgroundResource(R.mipmap.desk);
 
+        RelativeLayout.LayoutParams lp_fullWidth =
+                new RelativeLayout.LayoutParams(remain_width, remain_height);
+
+        lp_fullWidth.setMargins(margin,0,0,0);
 
         for(int i = 0; i < 6;i++){
             RelativeLayout.LayoutParams btParams = new  RelativeLayout.LayoutParams(hole_width,hole_width);
             if(i < 3){
-                btParams.setMargins(margin - hole_width/2, i*(remain_height/2),0,0);   //左上右下
-                System.out.println("di  "+ i +"  dian :  x : " + (margin - hole_width/2) + " y : " + i*(remain_height/3));
+               // btParams.setMargins(hole_margin - hole_width/2, i*(remain_height/2),0,0);   //左上右下
+                btParams.setMargins(lf_border - hole_width/2, to_border - hole_width/2 + i * (real_desk_hight/2),0,0);   //左上右下
+                System.out.println("di  "+ i +"  dian :  x : " + (lf_border - hole_width) + " y : " + (to_border - hole_width));
             }else{
-                btParams.setMargins(margin + remain_width - hole_width/2,(i-3)*(remain_height/2) ,0,0);   //左上右下
-                System.out.println("di  "+ i +"  dian :  x : " + (margin + remain_width - hole_width/2) + " y : " + (i-3)*(remain_height/3));
+                btParams.setMargins(lf_border + real_desk_width - hole_width/2, (to_border - hole_width/2) + (i - 3) * (real_desk_hight/2),0,0);   //左上右下
+
+                System.out.println("di  "+ i +"  dian :  x : " + (lf_border + real_desk_width - hole_width/2) + " y : " + (to_border - hole_width/2) + (i - 3) * (real_desk_hight/2));
             }
 
 
             holeButton[i] = new ImageButton(this);
             holeButton[i].setId(1200+i);
-            //holeButton[i].setImageResource(R.drawable.lan);
-            holeButton[i].setBackgroundResource(R.drawable.hole);
+            //holeButton[i].setImageResource(R.mipmap.lan);
+            holeButton[i].setBackgroundResource(R.mipmap.hole);
             //holeButton[i].setScaleType(ImageView.ScaleType.CENTER );
             //btParams.addRule(RelativeLayout.BELOW, 1);
             lay.addView(holeButton[i],btParams);        //将按钮放入layout组件
@@ -256,19 +281,24 @@ public class MainActivity extends Activity {
                 String[] valArray = ma.get(key).split("\\@");
 
                 int  x = 0,y = 0,j = 0;
+
                 boolean isWhite = false;
                 for(String s : valArray){
                     if(j == 0 ){
                         coordinate[tot][0] = s;
-                        y = (int)((Integer.parseInt(s)/1366.0f) * remain_height) + 5;
+                        y = (int)((Integer.parseInt(s)/1366.0f) * real_desk_hight) + to_border;
                     }else if(j == 1){
 
                         coordinate[tot][1] = s;
-                        x = (int)((Integer.parseInt(s)/667.0f) * remain_width) + margin;
+                        x = (int)((Integer.parseInt(s)/667.0f) * real_desk_width) + lf_border;
                     }else if(j == 2){
-                        continue;
+                        System.out.println("j=2 s= "+s);
+
                     }else if(j == 3){
+                        System.out.println("j=3 s= "+s);
+                        System.out.println(s.equals("white"));
                         if(s.equals("white")){
+                            System.out.println("isWhite "+isWhite);
                             isWhite = true;
                         }
                         break;
@@ -279,15 +309,19 @@ public class MainActivity extends Activity {
                 Btn[tot]=new ImageButton(this);
                 Btn[tot].setId(2000+tot);
 
-                if(isWhite)
-                    Btn[tot].setBackgroundResource(R.drawable.white);
-                else
-                    Btn[tot].setBackgroundResource(R.drawable.ball);
+                if(isWhite){
+                    Btn[tot].setBackgroundResource(R.mipmap.white);
+                    white_id = tot;
+                }
+                else{
+                    Btn[tot].setBackgroundResource(R.mipmap.ball);
+                }
+
 
                 //Btn[tot].setScaleType(ImageView.ScaleType.CENTER );
-                RelativeLayout.LayoutParams btParams = new  RelativeLayout.LayoutParams(button_width,button_height);
+                RelativeLayout.LayoutParams btParams = new  RelativeLayout.LayoutParams(button_width,button_width);
 
-                btParams.setMargins(x - button_width/2, y - button_height/2,0,0);   //左上右下
+                btParams.setMargins(x - button_width/2, y - button_width/2,0,0);   //左上右下
 
                 //btParams.addRule(RelativeLayout.BELOW, 1);
                 lay.addView(Btn[tot],btParams);        //将按钮放入layout组件
@@ -296,25 +330,33 @@ public class MainActivity extends Activity {
             }
         }
 
+
         layout.addView(lay,lp_fullWidth);
+
         for( int k = 0; k <= tot-1; k++) {
+
+            if(k == white_id)
+                continue;
+
             //这里不需要findId，因为创建的时候已经确定哪个按钮对应哪个Id
             Btn[k].setTag(k);    //为按钮设置一个标记，来确认是按下了哪一个按钮
             Btn[k].setOnClickListener(new Button.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
+
                     ImageButton confirm_button = (ImageButton)findViewById(R.id.confirm_button);
                     btnid = v.getId() - 2000;
                    // if(btnid != enable_id){
                         if(enable_id != -1){
-                            Btn[enable_id].setBackgroundResource(R.drawable.ball);
+                            Btn[enable_id].setBackgroundResource(R.mipmap.ball);
                         }
 
                         ball_enable = true;
                         ImageButton temp = (ImageButton) v;
-                        temp.setBackgroundResource(R.drawable.ball_on);
+                        temp.setBackgroundResource(R.mipmap.ball_on);
                          confirm_button.setClickable(true);
-                         confirm_button.setImageResource(R.drawable.confirm_button_light);
+                         confirm_button.setImageResource(R.mipmap.confirm_button_light);
                         enable_id = btnid;
                   //  }
                 }
@@ -325,27 +367,22 @@ public class MainActivity extends Activity {
            holeButton[i].setOnClickListener(new Button.OnClickListener(){
                @Override
                public void onClick(View v) {
-
                    hole_id = v.getId() - 1200;
                    ImageButton confirm_button = (ImageButton)findViewById(R.id.confirm_button);
 
-                   //if(hole_id != enable_hole_id){
-
                        if(enable_hole_id != -1){
-                           holeButton[enable_hole_id].setBackgroundResource(R.drawable.hole);
+                           holeButton[enable_hole_id].setBackgroundResource(R.mipmap.hole);
                        }
                        ImageButton temp = (ImageButton) v;
-                       temp.setBackgroundResource(R.drawable.hole_on);
+                       temp.setBackgroundResource(R.mipmap.hole_on);
                        hole_enable = true;
                        confirm_button.setClickable(true);
-                       confirm_button.setImageResource(R.drawable.confirm_button_light);
+                       confirm_button.setImageResource(R.mipmap.confirm_button_light);
                        enable_hole_id = hole_id;
 
-                  // }
                }
            });
        }
-
 
         ImageButton confirm_button = (ImageButton)findViewById(R.id.confirm_button);
         confirm_button.setOnClickListener(new Button.OnClickListener(){
@@ -365,12 +402,12 @@ public class MainActivity extends Activity {
 
                     ImageButton temp = (ImageButton)v;
                     temp.setClickable(false);
-                    temp.setImageResource(R.drawable.confirm_button_dark);
+                    temp.setImageResource(R.mipmap.confirm_button_dark);
 
                     ball_enable = false;
                     hole_enable = false;
-                    holeButton[hole_id].setBackgroundResource(R.drawable.hole);
-                    Btn[btnid].setBackgroundResource(R.drawable.ball);
+                    holeButton[hole_id].setBackgroundResource(R.mipmap.hole);
+                    Btn[btnid].setBackgroundResource(R.mipmap.ball);
 
                 }
 
@@ -379,6 +416,7 @@ public class MainActivity extends Activity {
 
         confirm_button.setClickable(false);
     }
+
 
 }
 
